b7613a6914941ac18ee221b2e635bf8d425f4870
MASSAQUI <elisiomassaqui8@gmail.com>
2023-11-27 20:16:22 +0000

    Vida de J2

--- wandiController.cs
+++ wandiController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class wandiController : MonoBehaviour
 {
     public GameObject J2;
-    public float velocityJ2;
+    public float velocityJ2 = 0f;
+    public Slider sliderVelocityJ2;
+    public float limiteSuperiorJ2;
+    public float limiteInferiorJ2;
+    public float grauDaRotaçãoJ2 = 0f;
 
 
     // Start is called before the first frame update
