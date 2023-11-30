6dc4c074844244e962b380f382d96fd1d78e4b87
MASSAQUI <elisiomassaqui8@gmail.com>
2023-11-30 10:06:07 +0000

    mapeamento de joysticks

--- wandiController.cs
+++ wandiController.cs
@@ -10,7 +10,7 @@ public class wandiController : MonoBehaviour
 
     //Vida Da Nossa JuntaJ1
     public GameObject J1;
-    public float velocityJ1;
+    public float velocityJ1 = 0f;
     public Slider sliderVelocityJ1;
     public float limiteEsquerdoJ1;
     public float limiteDireitoJ1;
@@ -40,6 +40,7 @@ public class wandiController : MonoBehaviour
     void Update()
     {
         velocityJ2 = sliderVelocityJ2.value;
+        velocityJ1 = sliderVelocityJ2.value;
 
         if (Input.GetKey(KeyCode.Alpha2) && Input.GetKey(KeyCode.RightArrow))
         {
@@ -57,6 +58,86 @@ public class wandiController : MonoBehaviour
 
             Debug.Log("btnpress");
 
+        }
+
+        if (Input.GetKey(KeyCode.JoystickButton0)){
+            Debug.Log("Joy");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton1))
+        {
+            Debug.Log("Joy1");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton2))
+        {
+            Debug.Log("Joy2");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton3))
+        {
+            Debug.Log("Joy3");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton4))
+        {
+            Debug.Log("Joy4");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton5))
+        {
+            Debug.Log("Joy5");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton6))
+        {
+            Debug.Log("Joy6");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton7))
+        {
+            Debug.Log("Joy7");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton8))
+        {
+            Debug.Log("Joy8");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton9)) 
+        {
+            Debug.Log("Joy9");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton10))
+        {
+            Debug.Log("Joy10");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton11))
+        {
+            Debug.Log("Joy11");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton12))
+        {
+            Debug.Log("Joy12");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton13))
+        {
+            Debug.Log("Joy13");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton14))
+        {
+            Debug.Log("Joy14");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton15))
+        {
+            Debug.Log("Joy15");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton16))
+        {
+            Debug.Log("Joy16");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton17))
+        {
+            Debug.Log("Joy17");
+        }
+        if (Input.GetKey(KeyCode.JoystickButton18))
+        {
+            Debug.Log("Joy18");
+        }
+        if (Input.GetKey(KeyCode.Joystick2Button13))
+        {
+            Debug.Log("Joy20");
         }
     }
 
