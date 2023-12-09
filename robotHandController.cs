9167e02e7cf7f835d9a546c388d489548370bcb7
MASSAQUI <elisiomassaqui8@gmail.com>
2023-12-09 19:48:56 +0000

    variaveis arm e base

--- robotHandController.cs
+++ robotHandController.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class robotHandController : MonoBehaviour
 {
+    public float baseRate = 1;
+    public float armRate = 1;
+
+    public Slider sliderBase;
+    public Slider sliderArm;
+
+    public float baseYMin = -45.0f;
+    public float baseYMax = 45.0f;
+
+    public float armXMin = -45.0F;
+    public float armXMax = 45.0f;
+
     // Start is called before the first frame update
     void Start()
     {
