aba7eb21c7b15f2c3bee3fab4c2a8e5f17dcc755
MASSAQUI <elisiomassaqui8@gmail.com>
2023-12-09 21:25:47 +0000

    agora estou a entender bem em tempo real, depois tenho que comparar com outro codigo pra ver todas falhas anteriores

--- arm.cs
+++ arm.cs
@@ -11,15 +11,18 @@ public class arm : MonoBehaviour
     public float baseRate = 1;
     public float armRate = 1;
 
-    public Slider sliderBase = 0.0f;
-    public Slider sliderArm = 0.0f;
+    public Slider sliderBase;
+    public Slider sliderArm;
+
+    public float baseSliderValue = 0.0f;
+    public float armSliderValue = 0.0f;
 
     public float baseYRot = 0.0f;
     public float baseYMin = -45.0f;
     public float baseYMax = 45.0f;
 
     public float armXRot = 0.0f;
-    public float armXMin = -45.0F;
+    public float armXMin = -45.0f;
     public float armXMax = 45.0f;
 
     // Start is called before the first frame update
@@ -35,6 +38,25 @@ public class arm : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        baseSliderValue = sliderBase.value;
+        armSliderValue = sliderArm.value;
+
+        baseYRot += baseSliderValue * baseRate * Time.deltaTime;
+        baseYRot = Mathf.Clamp(baseYRot, baseYMin, baseYMax);
         
+        baseY.localEulerAngles = new Vector3(baseY.localEulerAngles.x, baseYRot, baseY.localEulerAngles.z);
+
+        armXRot += armSliderValue * armRate * Time.deltaTime;
+        armXRot = Mathf.Clamp(armXRot, armXMin, armXMax);
+
+        armX.localEulerAngles = new Vector3(armXRot, armX.localEulerAngles.y, armX.localEulerAngles.z);
+    }
+
+    public void resetSliders()
+    {
+        baseSliderValue = 0.0f;
+        armSliderValue = 0.0f;
+        sliderBase.value = 0.0f;
+        sliderArm.value = 0.0f;
     }
 }
