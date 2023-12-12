ebb5500842b46128cbe59fcf6b4660b6b3c02812
MASSAQUI <elisiomassaqui8@gmail.com>
2023-12-12 09:14:34 +0000

    falou j1

--- quaternion.cs
+++ quaternion.cs
@@ -7,12 +7,12 @@ public class quaternion : MonoBehaviour
 {
      //Vida J1
     public Transform J1; // Transformação do Nosso objecto!
-    public float velocidadeDaRotacaoJ1;
+    public float velocidadeJ1; // velocidade do nosso objecto!
     public Slider sliderJ1; // slider de controle de velocidade!
-    public float valorDoSliderJ1; // velocidade do nosso objecto!
     public float J1Min; // Valor Minimo da rotação!
     public float J1Max; // Valor Máximo da rotação!
     public float grausDaRotacaoJ1; // progresso da nossa rotação, ótimo pra exibir na tela, alias todo valor pode ser exibido em tempo real!
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +22,6 @@ public class quaternion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        valorDoSliderJ1 = sliderJ1.value;
 
         sliderJ1.minValue = -1;
         sliderJ1.maxValue = 1;
@@ -31,9 +30,8 @@ public class quaternion : MonoBehaviour
 
     public void UpdateJ1()
     {
-        grausDaRotacao += valorDoSliderJ1 * velocidadeDaRotacaoJ1 * Time.deltaTime;
-        grausDaRotacao = Mathf.Clamp(grausDaRotacao, J1Min, J1Max); 
-
-        
+        velocidadeJ1 = sliderJ1.value;
+        grausDaRotacaoJ1 += velocidadeJ1 * Time.deltaTime;
+        J1.localRotation = Quaternion.Euler(0f, 0f, grausDaRotacaoJ1);
     }
 }
