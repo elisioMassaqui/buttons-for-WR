1fd88515de71e2a2011ff6e457fda449a89555c0
MASSAQUI <elisiomassaqui8@gmail.com>
2023-12-12 08:46:56 +0000

    Vida J1

---
+++ quaternion.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class quaternion : MonoBehaviour
+{
+     //Vida J1
+    public Transform J1; // Transformação do Nosso objecto!
+    public float velocidadeJ1; // velocidade do nosso objecto!
+    public Slider sliderJ1; // slider de controle de velocidade!
+    public float J1Min; // Valor Minimo da rotação!
+    public float J1Max; // Valor Máximo da rotação!
+    public float grausDaRotacao; // progresso da nossa rotação, ótimo pra exibir na tela, alias todo valor pode ser exibido em tempo real!
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
