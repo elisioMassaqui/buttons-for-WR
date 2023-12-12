116eb6ed2c083553afe03f12af46f8b002d37ff5
MASSAQUI <elisiomassaqui8@gmail.com>
2023-12-12 09:32:27 +0000

    limites pra nossa rotação Y

--- quaternion.cs
+++ quaternion.cs
@@ -10,8 +10,8 @@ public class quaternion : MonoBehaviour
     public float velocidadeJ1; // Velocidade dda J1;
     public Slider sliderJ1; // slider de controle de velocidade!
     public float valorDoSliderJ1; // valor do nosso slider!
-    public float J1Min; // Valor Minimo da rotação!
-    public float J1Max; // Valor Máximo da rotação!
+    public float J1YMin; // Valor Minimo da rotação!
+    public float J1YMax; // Valor Máximo da rotação!
     public float RotationJ1X; // progresso da nossa rotaçãoX, ótimo pra exibir na tela, alias todo valor pode ser exibido em tempo real!
     public float RotationJ1Y; // progresso da nossa rotaçãoY, ótimo pra exibir na tela, alias todo valor pode ser exibido em tempo real!
     public float RotationJ1Z; // progresso da nossa rotaçãoZ, ótimo pra exibir na tela, alias todo valor pode ser exibido em tempo real!
@@ -34,6 +34,7 @@ public class quaternion : MonoBehaviour
     {
         valorDoSliderJ1 = sliderJ1.value;
         RotationJ1Y += valorDoSliderJ1 * velocidadeJ1 * Time.deltaTime;
+        RotationJ1Y = Mathf.Clamp(RotationJ1Y, J1YMin, J1YMax);
         J1.localRotation = Quaternion.Euler(RotationJ1X, RotationJ1Y, RotationJ1Z);
     }
 }
