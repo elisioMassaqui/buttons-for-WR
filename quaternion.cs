b01cd5dca84c127a42a2155e0dfe403b7c0382f6
MASSAQUI <elisiomassaqui8@gmail.com>
2023-12-12 09:25:59 +0000

    tem que ser o nosso o valor do nosso eixo mais valor do slider vezes velociade e vezes variação do tempo real

--- quaternion.cs
+++ quaternion.cs
@@ -7,12 +7,14 @@ public class quaternion : MonoBehaviour
 {
      //Vida J1
     public Transform J1; // Transformação do Nosso objecto!
-    public float velocidadeJ1; // velocidade do nosso objecto!
+    public float velocidadeJ1;
     public Slider sliderJ1; // slider de controle de velocidade!
+    public float valorDoSliderJ1; // velocidade do nosso objecto!
     public float J1Min; // Valor Minimo da rotação!
     public float J1Max; // Valor Máximo da rotação!
-    public float grausDaRotacaoJ1; // progresso da nossa rotação, ótimo pra exibir na tela, alias todo valor pode ser exibido em tempo real!
-    
+    public float RotationJ1X; // progresso da nossa rotação, ótimo pra exibir na tela, alias todo valor pode ser exibido em tempo real!
+    public float RotationJ1Y;
+    public float RotationJ1Z;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,8 +32,8 @@ public class quaternion : MonoBehaviour
 
     public void UpdateJ1()
     {
-        velocidadeJ1 = sliderJ1.value;
-        grausDaRotacaoJ1 += velocidadeJ1 * Time.deltaTime;
-        J1.localRotation = Quaternion.Euler(0f, 0f, grausDaRotacaoJ1);
+        valorDoSliderJ1 = sliderJ1.value;
+        RotationJ1Y += valorDoSliderJ1 * velocidadeJ1 * Time.deltaTime;
+        J1.localRotation = Quaternion.Euler(RotationJ1X, RotationJ1Y, RotationJ1Z);
     }
 }
