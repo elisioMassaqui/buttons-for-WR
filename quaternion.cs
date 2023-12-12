7e995bcece04930e63df4b8526a47e24daa34aa5
MASSAQUI <elisiomassaqui8@gmail.com>
2023-12-12 09:03:26 +0000

    Acrescentamos a velocidade da Rotação J1 e o valor do slider e colocamos as operações na função e colocamos limites

--- quaternion.cs
+++ quaternion.cs
@@ -7,12 +7,12 @@ public class quaternion : MonoBehaviour
 {
      //Vida J1
     public Transform J1; // Transformação do Nosso objecto!
-    public float velocidadeJ1; // velocidade do nosso objecto!
+    public float velocidadeDaRotacaoJ1;
     public Slider sliderJ1; // slider de controle de velocidade!
+    public float valorDoSliderJ1; // velocidade do nosso objecto!
     public float J1Min; // Valor Minimo da rotação!
     public float J1Max; // Valor Máximo da rotação!
-    public float grausDaRotacao; // progresso da nossa rotação, ótimo pra exibir na tela, alias todo valor pode ser exibido em tempo real!
-    
+    public float grausDaRotacaoJ1; // progresso da nossa rotação, ótimo pra exibir na tela, alias todo valor pode ser exibido em tempo real!
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +22,7 @@ public class quaternion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        velocidadeJ1 = sliderJ1.value;
+        valorDoSliderJ1 = sliderJ1.value;
 
         sliderJ1.minValue = -1;
         sliderJ1.maxValue = 1;
@@ -31,6 +31,7 @@ public class quaternion : MonoBehaviour
 
     public void UpdateJ1()
     {
-        
+        grausDaRotacao += valorDoSliderJ1 * velocidadeDaRotacaoJ1 * Time.deltaTime;
+        grausDaRotacao = Mathf.Clamp(grausDaRotacao, J1Min, J1Max); 
     }
 }
